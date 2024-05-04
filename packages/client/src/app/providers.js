7f56793e021dc35d9ac66471d1f71756eb4169c3
rogeriesmontsia <rogerarques@iesmontsia.org>
2024-05-04 21:09:55 +0200

    open dialog to sign message but returns 401

--- packages/client/src/app/providers.js
+++ packages/client/src/app/providers.js
@@ -6,6 +6,7 @@ import {
 } from '@rainbow-me/rainbowkit';
 import { WagmiProvider } from 'wagmi';
 import {
+    mainnet,
     sepolia,
     polygon,
     optimism,
@@ -16,23 +17,28 @@ import {
     QueryClientProvider,
     QueryClient,
 } from "@tanstack/react-query";
+import { SessionProvider, useSession } from 'next-auth/react';
+import { RainbowKitSiweNextAuthProvider, } from '@rainbow-me/rainbowkit-siwe-next-auth';
 
 export const config = getDefaultConfig({
     appName: 'My RainbowKit App',
     projectId: '46628dba1c5e4a4771f8dd3816df4d92',
-    chains: [sepolia, polygon, optimism, arbitrum, base],
+    chains: [mainnet, sepolia, polygon, optimism, arbitrum, base],
     ssr: true, // If your dApp uses server side rendering (SSR)
 });
 
 const queryClient = new QueryClient();
 
 export function Providers({ children }) {
-
     return (
         <WagmiProvider config={config} >
-            <QueryClientProvider client={queryClient}>
-                <RainbowKitProvider>{children}</RainbowKitProvider>
-            </QueryClientProvider>
+            <SessionProvider>
+                <QueryClientProvider client={queryClient}>
+                    <RainbowKitSiweNextAuthProvider>
+                        <RainbowKitProvider>{children}</RainbowKitProvider>
+                    </RainbowKitSiweNextAuthProvider>
+                </QueryClientProvider>
+            </SessionProvider>
         </WagmiProvider>
     );
 }
