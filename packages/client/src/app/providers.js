e68f56807898031f31c647a8872a529a03ae6be0
rogeriesmontsia <rogerarques@iesmontsia.org>
2024-05-04 22:00:52 +0200

    Revert "open dialog to sign message but returns 401"
    This reverts commit 7f56793e021dc35d9ac66471d1f71756eb4169c3.

--- packages/client/src/app/providers.js
+++ packages/client/src/app/providers.js
@@ -6,7 +6,6 @@ import {
 } from '@rainbow-me/rainbowkit';
 import { WagmiProvider } from 'wagmi';
 import {
-    mainnet,
     sepolia,
     polygon,
     optimism,
@@ -17,28 +16,23 @@ import {
     QueryClientProvider,
     QueryClient,
 } from "@tanstack/react-query";
-import { SessionProvider, useSession } from 'next-auth/react';
-import { RainbowKitSiweNextAuthProvider, } from '@rainbow-me/rainbowkit-siwe-next-auth';
 
 export const config = getDefaultConfig({
     appName: 'My RainbowKit App',
     projectId: '46628dba1c5e4a4771f8dd3816df4d92',
-    chains: [mainnet, sepolia, polygon, optimism, arbitrum, base],
+    chains: [sepolia, polygon, optimism, arbitrum, base],
     ssr: true, // If your dApp uses server side rendering (SSR)
 });
 
 const queryClient = new QueryClient();
 
 export function Providers({ children }) {
+
     return (
         <WagmiProvider config={config} >
-            <SessionProvider>
-                <QueryClientProvider client={queryClient}>
-                    <RainbowKitSiweNextAuthProvider>
-                        <RainbowKitProvider>{children}</RainbowKitProvider>
-                    </RainbowKitSiweNextAuthProvider>
-                </QueryClientProvider>
-            </SessionProvider>
+            <QueryClientProvider client={queryClient}>
+                <RainbowKitProvider>{children}</RainbowKitProvider>
+            </QueryClientProvider>
         </WagmiProvider>
     );
 }
